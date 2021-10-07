da6cd6c62b0d96825eecbe701aae23ec7de21e6e
Asad Elahi <asad1234go@gmail.com>
2021-10-07 10:45:37 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+"""
+setup.py
+
+Used by: python setup.py nosetests
+"""
+from setuptools import setup
+
+setup(name='PyUpdater App',
+      version='1.2.0',
+      description='PyUpdater wxPython demo',
+      url='http://github.com/wettenhj/pyupdater-wx-demo',
+      author='James Wettenhall',
+      author_email='james.wettenhall@monash.edu',
+      license='BSD',
+      packages=['PyUpdater App'],
+      zip_safe=False)
